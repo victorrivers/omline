e7aea385cdbedaa7ab258887050c2b15a3d4414a
Victor Rios <victor_github@outlook.com>
2023-07-26 23:07:09 +0200

    Bell audio 🛎

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,5 +1,10 @@
 import styles from "./menu.module.scss";
 
 export function Menu() {
-	return <h1 className={styles.menu}>omline</h1>;
+	return (
+		<>
+			<h1 className={styles.menu}>omline</h1>
+			<audio controls src="./audio/bell.mp3"></audio>
+		</>
+	);
 }
