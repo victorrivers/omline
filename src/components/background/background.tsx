39fce002ebd8b30b44bf879b7c80dac819113f89
Victor Rios <victor_github@outlook.com>
2023-07-26 22:50:52 +0200

    Background image updated

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
 import styles from "./background.module.scss";
 
 export function Background(): JSX.Element {
-	return (
-		<div className={styles.background}>
-			<Image src="/mist.gif" alt="Mist" priority fill />
-		</div>
-	);
+	return <div className={styles.background} />;
 }
